bb9eb930e43b29ed7cc2e194bf53cb324cb9f877
KeoChamraeun <keochamraeun54@gmail.com>
2025-05-26 15:37:20 +0700

    update all

--- app/Http/Livewire/Admin/Subcategory/Create.php
+++ app/Http/Livewire/Admin/Subcategory/Create.php
@@ -32,7 +32,7 @@ class Create extends Component
         'subcategory.name'        => ['required', 'string', 'max:255'],
         'subcategory.category_id' => ['nullable', 'integer'],
         'subcategory.language_id' => ['nullable'],
-        'image' => ['nullable', 'image', 'max:2048'], // 2MB max
+        // 'image' => ['nullable', 'image', 'max:2048'], // 2MB max
     ];
 
     public function render(): View|Factory
